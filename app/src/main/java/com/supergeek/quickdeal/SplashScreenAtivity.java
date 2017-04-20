4c2d8e40b2efbad6b334f32462d8afd6fa6389d2
shubhamjuneja11 <shubhamjuneja11@gmail.com>
2017-04-20 18:49:14 +0530

    Initial commit

---
+++ app/src/main/java/com/supergeek/quickdeal/SplashScreenAtivity.java
@@ -0,0 +1,13 @@
+package com.supergeek.quickdeal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenAtivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen_ativity);
+    }
+}
