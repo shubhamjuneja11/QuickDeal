872d63c24a883766d8bbe249a7e18772153e7aca
shubhamjuneja11 <shubhamjuneja11@gmail.com>
2017-04-20 21:31:21 +0530

    main xml created

--- app/src/main/java/com/supergeek/quickdeal/MainActivity.java
+++ app/src/main/java/com/supergeek/quickdeal/MainActivity.java
@@ -3,11 +3,11 @@ package com.supergeek.quickdeal;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashScreenAtivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen_ativity);
+        setContentView(R.layout.activity_main);
     }
 }
---
+++ app/src/main/java/com/supergeek/quickdeal/SplashScreenActivity.java
@@ -0,0 +1,33 @@
+package com.supergeek.quickdeal;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+int splash_time=3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen_ativity);
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, splash_time);
+    }
+}
