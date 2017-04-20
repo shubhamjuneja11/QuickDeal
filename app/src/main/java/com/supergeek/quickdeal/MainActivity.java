7df0aab0856598acf7f32583a5cdbe636d5e7a5c
shubhamjuneja11 <shubhamjuneja11@gmail.com>
2017-04-21 00:40:33 +0530

    Buyer activity done

--- app/src/main/java/com/supergeek/quickdeal/MainActivity.java
+++ app/src/main/java/com/supergeek/quickdeal/MainActivity.java
@@ -2,12 +2,23 @@ package com.supergeek.quickdeal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.seller:
+                break;
+            case R.id.buyer:
+                break;
+        }
     }
 }
